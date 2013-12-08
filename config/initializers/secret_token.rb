28d570c4b67efaea2255cbc1ec8ed12f283e81a1
Zahid <kronosmes@gmail.com>
2013-12-08 02:03:22 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testgeolocate::Application.config.secret_token = '0335e0dd009966e8234ecca7d56c0387ae6b2a67ec1fb23ca2b3066cbb71cc76e3d4df285b88e518879f92988d31b67e747d7b67ae6240e0e732edb55cb70d74'
